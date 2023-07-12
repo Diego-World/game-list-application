c027422d49d60948af0814de4e561a8825f98fc0
Diego Gouveia <contato.diegogouveia@gmail.com>
2023-07-12 18:41:02 -0300

    Implementado função de buscar por ID

--- src/main/java/com/diegogouveia/dslist/controllers/GameController.java
+++ src/main/java/com/diegogouveia/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.diegogouveia.dslist.controllers;
 
+import com.diegogouveia.dslist.dto.GameDTO;
 import com.diegogouveia.dslist.dto.GameMinDTO;
 import com.diegogouveia.dslist.entities.Game;
 import com.diegogouveia.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,13 @@ import java.util.List;
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
     @GetMapping
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> dto = gameService.findaAll();
--- src/main/java/com/diegogouveia/dslist/services/GameService.java
+++ src/main/java/com/diegogouveia/dslist/services/GameService.java
@@ -1,21 +1,38 @@
 package com.diegogouveia.dslist.services;
 
+import com.diegogouveia.dslist.dto.GameDTO;
 import com.diegogouveia.dslist.dto.GameMinDTO;
 import com.diegogouveia.dslist.entities.Game;
 import com.diegogouveia.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+// Responsável por armazenar regras de negócio.
 
 @Service
 public class GameService {
     @Autowired // Injeção de dependência (Componente = service)
     private GameRepository gameRepository;
+
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        //Aqui cabe um tratamento de exception caso o id não exista por exemplo.
+        return new GameDTO(result);
+//        return dto;
+    }
+
     public List<GameMinDTO> findaAll(){
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x->new GameMinDTO(x)).toList();
 //        return result.stream().map(x->new GameMinDTO(x)).toList(); Também pode ser retornado dessa forma
         return dto;
     }
+
+
+
 }
